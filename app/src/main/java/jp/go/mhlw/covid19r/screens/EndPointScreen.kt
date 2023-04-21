03acfdbdd89b8427e1a05bbd43757c81dbe8f67a
Oleg Vishnevskiy <vishnevskiyoleg@gmail.com>
2023-04-21 09:02:19 +0300

    can add files to web pages

--- app/src/main/java/jp/go/mhlw/covid19r/screens/EndPointScreen.kt
+++ app/src/main/java/jp/go/mhlw/covid19r/screens/EndPointScreen.kt
@@ -10,12 +10,15 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.os.Message
 import android.provider.MediaStore
 import android.util.Log
 import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import jp.go.mhlw.covid19r.Const
 import jp.go.mhlw.covid19r.R
 import jp.go.mhlw.covid19r.databinding.ActivityEndPointScreenBinding
@@ -25,157 +28,83 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 
-private const val FCR = 1
 
 class EndPointScreen : AppCompatActivity() {
 
     private lateinit var binding: ActivityEndPointScreenBinding
-    private var mCM: String? = null
-    private var mUM: ValueCallback<Uri>? = null
-    private var mUMA: ValueCallback<Array<Uri>>? = null
+    private lateinit var chooseCallback: ValueCallback<Array<Uri?>>
+    val getContent = registerForActivityResult(ActivityResultContracts.GetMultipleContents()){
+        chooseCallback.onReceiveValue(it.toTypedArray())
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEndPointScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
         window.statusBarColor = getColor(R.color.black)
-        setWebView()
+        setWebClicks(binding.myView)
 
-    }
-
-    override fun onBackPressed() {
-        if(binding.myView.canGoBack()){
-            binding.myView.goBack()
-        }
-    }
-
-    @SuppressLint("SetJavaScriptEnabled")
-    private fun setWebView() {
-        val webView = binding.myView
-        webView.settings.javaScriptEnabled = true
-        webView.settings.loadWithOverviewMode = true
-        webView.settings.useWideViewPort = true
-        webView.settings.domStorageEnabled = true
-        webView.settings.allowFileAccess=true
-        webView.settings.allowContentAccess=true
-        webView.settings.javaScriptCanOpenWindowsAutomatically=true
 
         val link = intent.extras?.getString(Const.SHARED_LINK_NAME).toString()
         //https://ru.imgbb.com/
+        binding.myView.loadUrl("https://ru.imgbb.com/")
 
-        webView.loadUrl("https://ru.imgbb.com/")
-        webView.webChromeClient = object : WebChromeClient() {
+
+        binding.myView.webViewClient = object : WebViewClient(){
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                //Сохранить в sharedpref адрес конечной ссылки
+            }
+        }
+
+        with(binding.myView.settings){
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            loadWithOverviewMode = false
+        }
+
+        binding.myView.webChromeClient = object : WebChromeClient(){
             override fun onShowFileChooser(
-                webView: WebView,
-                filePathCallback: ValueCallback<Array<Uri>>,
-                fileChooserParams: FileChooserParams
+                webView: WebView?,
+                filePathCallback: ValueCallback<Array<Uri?>>,
+                fileChooserParams: FileChooserParams?
             ): Boolean {
-                if (mUMA != null) {
-                    mUMA!!.onReceiveValue(null)
+                chooseCallback = filePathCallback
+                getContent.launch("image/*")
+                return true
+            }
+
+            override fun onCreateWindow(
+                view: WebView?,
+                isDialog: Boolean,
+                isUserGesture: Boolean,
+                resultMsg: Message
+            ): Boolean {
+                val createdWV = WebView(this@EndPointScreen)
+                with(createdWV.settings){
+                    javaScriptEnabled = true
+                    javaScriptCanOpenWindowsAutomatically = true
+                    domStorageEnabled = true
+                    setSupportMultipleWindows(true)
                 }
-                mUMA = filePathCallback
-                var takePictureIntent: Intent? = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                if (takePictureIntent!!.resolveActivity(getPackageManager()) != null) {
-                    var photoFile: File? = null
-                    try {
-                        photoFile = createImageFile()
-                        takePictureIntent.putExtra("PhotoPath", mCM)
-                    } catch (ex: IOException) {
-                        Log.e("Webview", "Image file creation failed", ex)
-                    }
-                    if (photoFile != null) {
-                        mCM = "file:" + photoFile.getAbsolutePath()
-                        takePictureIntent.putExtra(
-                            MediaStore.EXTRA_OUTPUT,
-                            Uri.fromFile(photoFile)
-                        )
-                    } else {
-                        takePictureIntent = null
-                    }
-                }
-                val contentSelectionIntent = Intent(Intent.ACTION_GET_CONTENT)
-                contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE)
-                contentSelectionIntent.type = "*/*"
-                val intentArray: Array<Intent>
-                intentArray = takePictureIntent?.let { arrayOf(it) } ?: arrayOf<Intent>()
-                val chooserIntent = Intent(Intent.ACTION_CHOOSER)
-                chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent)
-                chooserIntent.putExtra(Intent.EXTRA_TITLE, "Image Chooser")
-                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray)
-                startActivityForResult(chooserIntent, FCR)
+                createdWV.webChromeClient = this
+                val trans = resultMsg.obj as WebView.WebViewTransport
+                trans.webView = createdWV
+                resultMsg.sendToTarget()
                 return true
             }
         }
     }
 
-
-    @Throws(IOException::class)
-    private fun createImageFile(): File? {
-        @SuppressLint("SimpleDateFormat") val timeStamp: String =
-            SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val imageFileName = "img_" + timeStamp + "_"
-        val storageDir: File =
-            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile(imageFileName, ".jpg", storageDir)
-    }
-
-    fun openFileChooser(
-        uploadMsg: ValueCallback<Uri?>?,
-        acceptType: String?
-    ) {
-        this.openFileChooser(uploadMsg, acceptType, null)
-    }
-
-    fun openFileChooser(
-        uploadMsg: ValueCallback<Uri?>?,
-        acceptType: String?,
-        capture: String?
-    ) {
-        val i = Intent(Intent.ACTION_GET_CONTENT)
-        i.addCategory(Intent.CATEGORY_OPENABLE)
-        i.type = "*/*"
-        startActivityForResult(
-            Intent.createChooser(i, "File Browser"),
-            FCR
-        )
-    }
-
-    override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        intent: Intent?
-    ) {
-        super.onActivityResult(requestCode, resultCode, intent)
-        if (Build.VERSION.SDK_INT >= 21) {
-            var results: Array<Uri>? = null
-            //Check if response is positive
-            if (resultCode == Activity.RESULT_OK) {
-                if (requestCode == FCR) {
-                    if (null == mUMA) {
-                        return
-                    }
-                    if (intent == null) { //Capture Photo if no image available
-                        if (mCM != null) {
-                            results = arrayOf(Uri.parse(mCM))
-                        }
-                    } else {
-                        val dataString = intent.dataString
-                        if (dataString != null) {
-                            results = arrayOf(Uri.parse(dataString))
-                        }
+    fun setWebClicks(wv : WebView){
+        onBackPressedDispatcher.addCallback(this,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    if (wv.canGoBack()) {
+                        wv.goBack()
                     }
                 }
-            }
-            mUMA!!.onReceiveValue(results)
-            mUMA = null
-        } else {
-            if (requestCode == FCR) {
-                if (null == mUM) return
-                val result =
-                    if (intent == null || resultCode != Activity.RESULT_OK) null else intent.data
-                mUM!!.onReceiveValue(result)
-                mUM = null
-            }
-        }
+            })
     }
 }
